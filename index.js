98b7cf3546dcff93f9bb4640b50f184f0d30bf67
KBazina <kbazina@unipu.hr>
2024-10-22 09:48:07 +0200

    ama zadatak 1 vjezba 1

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const e = require("express")
-const express = require("express")
+const e = require("express");
+const express = require("express");
+const useri = [{ ime: "karlo", prezime: "bazina" },{ ime: "gabi", prezime: "nadal" },{ ime: "prof", prezime: "blasko" }];
+let app = express();
+const PORT = 3000;
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
-let app = express()
-const PORT = 3000
-app.get("/",(req,res)=>{
-    res.send("hello svijete")
-})
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/public/drugi.html");
+});
 
-app.listen(PORT,(error)=>{
-    if(error){
-        console.error(error)
-    } else {
-        console.log("slusam na ovom portu:", PORT)
-    }
-})
+app.get("/users", (req, res) => {
+  res.json(useri)
+});
+app.listen(PORT, (error) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log("slusam na ovom portu:", PORT);
+  }
+});
